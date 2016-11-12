0ee8e974fed8cf1c0bae140afe5956febc4be1cf
Simply Codin <shawnadams@outlook.com>
2016-11-12 09:22:31 -0600

    🎉Setup🎉
    Setup functions and Standard Firma

---
+++ Arduino-Metronome/Arduino-Metronome.ino
@@ -0,0 +1,27 @@
+void setup() {
+  // put your setup code here, to run once:
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  SFsetup();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  SFloop();
+  int bmp;
+  int input;
+  input = analogRead(0);
+  bmp = BeatsPerMin(input);
+  Serial.println(bmp);
+  delay(1);        // delay in between reads for stability
+}
+  int BeatsPerMin(int input){
+    //input = 0 to 1023
+    int result;  
+    //result = 60 to 120
+    result = 6;
+    0 + 120 * (input / 1023);
+    int delay;
+    delay = result * 1000;
+    return result;
+}

ca295dcb8e0addf6d00a2b85a5cc4a92a8a81577
Simply Codin <SimplyCodin@users.noreply.github.com>
2016-11-14 21:09:07 -0600

    Completed Code and Decluttered

--- Arduino-Metronome/Arduino-Metronome.ino
+++ Arduino-Metronome/Arduino-Metronome.ino
@@ -1,28 +1,30 @@
+int bmp;
+int delay;
 void setup() {
   // put your setup code here, to run once:
   // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
-  SFsetup();
-  int delay  delay;
+  // Serial.begin(9600);
+  // SFsetup();
+  pinMode(3, OUTPUT)
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  SFloop();
-  int bmp;
-  int input;
-  input = analogRead(A0);
-  bmp = BeatsPerMin(input);
-  Serial.println(bmp);
-  delay(1);        // delay in between reads for stability
+  //  SFloop(); 
+  bmp = BeatsPerMin(analogRead(A0));
+//  Serial.println(bmp);
+//  delay(1);        // delay in between reads for stability
+  delay = 3600 / bmp;
+  digitalWrite(3, HIGH);
+  digitalWrite(3, LOW);
+  delay(delay);
 }
   int BeatsPerMin(int input){
     //input = 0 to 1023
     int result;  
-    //result = 60 to 120
-    result = 6;
-    0 + 120 * (input / 1023);
+    //result = 60 to 10
+    result = 60 + 120 * (input / 1023);
     int delay;
     delay = result * 1000;
     return result;
